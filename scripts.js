405f488e4b152b328d85a2ee1e2ab9c2423939a2
maaazi643 <adeyemimuaz101@gmail.com>
2024-05-31 18:11:48 +0100

    Extracted Weather Information

--- scripts.js
+++ scripts.js
@@ -1,13 +1,35 @@
-const fetchWeatherButton = document.querySelector('.fetchWeatherButton');
+const fetchWeatherButton = document.querySelector(".fetchWeatherButton");
 
-fetchWeatherButton.addEventListener('click', getWeatherData)
+//Get City name
 
-function getWeatherData() {
+const apiKey = "d0d7df051cc6e6f5a2a9e229a1f07f43";
+
+const getWeatherData = async () => {
   //Get City name
-  const cityName = document.querySelector('.cityInput').value
+  const cityName = document.querySelector(".cityInput").value;
+  const apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},uk&APPID=${apiKey}`;
+  try {
+    const res = await fetch(apiURL);
+    console.log("Response status:", res.status);
+    if (!res.ok) {
+      throw new Error('Network response was not okay ' + res.statusText)
+    }
+    const data = await res.json();
 
-  //Make API Call
-  const apiKey = 'd0d7df051cc6e6f5a2a9e229a1f07f43'
-  const apiURL = `api.openweathermap.org/data/2.5/weather?q=${cityName},uk&APPID=${apiKey}`
+    console.log(data);
+  } catch (error) {
+    // throw new Error('Failed to fetch Data:' + error)
+    console.error(error)
+  }
+};
+
+const displayWeatherData = (data) => {
+  //Extract Weather Information
+  const temperature = data.main.temp;
+  const description = data.weather[0].description;
+  const humidity = data.main.humidity;
+
 
 }
+
+fetchWeatherButton.addEventListener("click", getWeatherData);
