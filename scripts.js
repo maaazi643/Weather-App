ddffec3115bf6d8f920954aa91421e82d7400ada
maaazi643 <adeyemimuaz101@gmail.com>
2024-06-02 03:10:18 +0100

    added somem styling

--- scripts.js
+++ scripts.js
@@ -1,43 +1,55 @@
 const fetchWeatherButton = document.querySelector(".fetchWeatherButton");
 
-//Get City name
-
 const apiKey = "d0d7df051cc6e6f5a2a9e229a1f07f43";
 
 const getWeatherData = async () => {
-  //Get City name
+  document.querySelector(".loadingMessage").style.display = "block";
+
   const cityName = document.querySelector(".cityInput").value;
   const apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},uk&APPID=${apiKey}`;
+
   try {
     const res = await fetch(apiURL);
     console.log("Response status:", res.status);
     if (!res.ok) {
-      throw new Error("Network response was not okay " + res.statusText);
+      displayError("Network response was not okay " + res.statusText);
     }
     const data = await res.json();
     displayWeatherData(data);
     console.log(data);
   } catch (error) {
-    // throw new Error('Failed to fetch Data:' + error)
-    console.error(error);
+    displayError("Failed to fetch Data: Check Internet Connection");
+  } finally {
+    document.querySelector(".loadingMessage").style.display = "none";
   }
 };
 
 const displayWeatherData = (data) => {
-  //Extract Weather Information
   const temperature = data.main.temp;
   const description = data.weather[0].description;
   const humidity = data.main.humidity;
+  const feelsLike = data.main.feels_like;
+  const pressure = data.main.pressure;
+  const windSpeed = data.wind.speed;
+  const maxTemp = data.main.temp_max;
+  const minTemp = data.main.temp_min;
 
-  //Update HTML to display weatherInformation
-  const weatherInformation = document.querySelector(".weatherDisplay");
-  weatherInformation.innerHTML = `
-    <p>Temperature: ${temperature} K</p>
-    <p>Description: ${description}</p>
-    <p>Humidity: ${humidity}%</p>
+  const weatherDisplay = document.querySelector(".weatherDisplay");
+  weatherDisplay.innerHTML = `
+    <p><img src="https://img.icons8.com/color/48/000000/temperature.png" alt="Temperature"> Temperature: ${temperature} K</p>
+    <p><img src="https://img.icons8.com/?size=100&id=W8fUZZSmXssu&format=png&color=000000" alt="Description"> Description: ${description}</p>
+    <p><img src="https://img.icons8.com/color/48/000000/humidity.png" alt="Humidity"> Humidity: ${humidity}%</p>
+    <p><img src="https://img.icons8.com/color/48/000000/temperature.png" alt="Feels Like"> Feels like: ${feelsLike}°C</p>
+    <p><img src="https://img.icons8.com/color/48/000000/pressure.png" alt="Pressure"> Pressure: ${pressure}</p>
+    <p><img src="https://img.icons8.com/color/48/000000/wind.png" alt="Wind Speed"> Wind Speed: ${windSpeed} m/s</p>
+    <p><img src="https://img.icons8.com/color/48/000000/temperature.png" alt="Max Temperature"> Max Temperature: ${maxTemp} K</p>
+    <p><img src="https://img.icons8.com/color/48/000000/temperature.png" alt="Min Temperature"> Min Temperature: ${minTemp} K</p>
   `;
 };
 
-
+const displayError = (message) => {
+  const weatherDisplay = document.querySelector(".weatherDisplay");
+  weatherDisplay.innerHTML = `Error: ${message}`;
+};
 
 fetchWeatherButton.addEventListener("click", getWeatherData);
